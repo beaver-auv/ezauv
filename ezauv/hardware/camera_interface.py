2026e1c0f1b9ee945c526ddac6d6e8914abde81c
McMatthew Adam <agua@computr.local>
2026-01-04 12:38:03 -0500

    New camera stuff

--- ezauv/hardware/camera_interface.py
+++ ezauv/hardware/camera_interface.py
@@ -1,5 +1,6 @@
 # OpenCV (Open computer vision) is the backend for this fantastic code.
 import cv2 as cv
+from ultralytics import YOLO
 from ezauv.utils import LogLevel
 import sys
 
@@ -12,10 +13,17 @@ CameraIndex is returned into cv's VideoCapture. This also means that you can spe
 
 
 class CameraObject:
-    def __init__(self, log: callable, camera, DivsionFactor):
+    def __init__(
+        self, log: callable, camera, DivsionFactor, framerate, UseModel, model
+    ):
         self.log = log
         camera = cv.VideoCapture(self.CameraIndex)
         DivisionFactor = self.DivisionFactor
+        framerate = self.framerate
+        UseModel = self.UseModel
+        model = self.model
+
+        cv.set(cv.CAP_PROP_FPS, framerate)
 
         # If the user chooses to use DivisionFactor to reduce the camera resolution, floor divide the width and height
         if DivisionFactor > 1:
@@ -33,10 +41,31 @@ class CameraObject:
                 LogLevel=LogLevel.WARNING,
             )
 
+        # This is much faster than the pervious code because frame() is run in a loop in the main code so we only check if the camera is working once.
+        # It might seem wierd because the camera could theoreticly fail at any time but this is faster.
+        ret, frame = self.camera.read()
+        if not ret:
+            self.log("ERROR: Can not read camera. The program will now exit.")
+            sys.exit(1)
+
+        # Load da model if there is one!
+        # We should also find some way to check if the sub is under remote control and if it is then we can unload the model. Clogs needs to get on that RC!
+        if UseModel:
+            self.log(
+                "Loading YOLO model. This will have a preformance impact. Make sure that you computer can keep up with the framerate"
+            )
+            model = YOLO(str(self.model))
+        else:
+            self.log("Not using YOLO")
+
     # Only get the frame when the user asks for it. This is MUCH faster then getting every frame.
     def frame(self):
         ret, frame = self.camera.read()
-        if not ret:
-            self.log("Camera error.")
-            sys.exit(1)
         return frame
+
+    # Same thing but allow loading in a YOLO model. By adding this as another function we can skip a theoretical if check in the above.
+    # Also load the module in __init__ because this is much faster than loading and unloading the model every time in one fuction
+    def frame_yolo(self):
+        ret, frame = self.camera.read()
+        results = self.model(frame)
+        return frame, results
